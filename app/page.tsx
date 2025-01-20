ee6eabc2175538d488aca3d8523dcebba2e2350e
Chaithana Chawalit <Chaithana_cha@cmc.co.th>
2025-01-20 15:49:57 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export default function page() {
+  const [data, setData] = useState();
+  return (
+    <div>
+      <div className="text-red-500">test2</div>
+      home
+    </div>
+  );
+}
