8163f6dea467959422a86dd121518c17f29660fe
Chaithana <chaithana13@hotmail.com>
2025-01-21 13:15:46 +0700

    updatea page bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
 "use client"
-
-import { useState, useEffect } from "react"
-
 export default function page() {
-  const [data, setData] = useState()
   return (
     <div>
       <div className="text-red-500">test2</div>
